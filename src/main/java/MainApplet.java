0b88080d9f0546de014dd686a3b4f3f174783edd
SherryChou <sherry18030@gmail.com>
2016-05-05 15:41:14 +0800

    MainApplet v1

--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,6 +1,10 @@
 package main.java;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
 
 /**
 * This class is for sketching outcome using Processing
@@ -9,24 +13,76 @@ import processing.core.PApplet;
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
 	private String path = "main/resources/";
-	private String file = "starwars-episode-1-interactions.json";
+	private String[] file = {"starwars-episode-1-interactions.json", "starwars-episode-2-interactions.json", 
+							"starwars-episode-3-interactions.json", "starwars-episode-4-interactions.json", 
+							"starwars-episode-5-interactions.json", "starwars-episode-6-interactions.json", 
+							"starwars-episode-7-interactions.json"};
 	
 	private final static int width = 1200, height = 650;
+	private ArrayList<Character> characters;
 	
 	public void setup() {
-
 		size(width, height);
+		characters = new ArrayList<Character>();
 		smooth();
 		loadData();
-		
 	}
 
 	public void draw() {
-
+		background(255);
+	}
+	
+	/* Change episode */
+	public void keyPressed(){
+		if(key == '1'){
+			
+		}
+		else if(key == '2'){
+			
+		}
+		else if(key == '3'){
+			
+		}
+		else if(key == '4'){
+			
+		}
+		else if(key == '5'){
+			
+		}
+		else if(key == '6'){
+			
+		}
+		else if(key == '7'){
+			
+		}
 	}
 
 	private void loadData(){
-
+		JSONArray nodes, links;
+		JSONObject data, tmp;
+		int i, episode;
+		
+		for(episode=0; episode<7; episode++){
+			data = loadJSONObject(path+file[episode]);
+			nodes = data.getJSONArray("nodes");
+			links = data.getJSONArray("links");
+			for(i=0; i<nodes.size(); i++){
+				tmp = nodes.getJSONObject(i);
+				
+				String n = tmp.getString("name");
+				int v = tmp.getInt("value");
+				String c = tmp.getString("colour");
+				
+	//			characters.add(new Character(this, n, x, y));
+			}
+			for(i=0; i<links.size(); i++){
+				tmp = links.getJSONObject(i);
+				int s = tmp.getInt("source");
+				int t = tmp.getInt("target");
+				int v = tmp.getInt("value");
+	//			characters.get(s).addTarget(characters.get(t));
+			}
+		}
 	}
 
 }
