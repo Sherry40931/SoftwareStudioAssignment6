8fa1801380505b2cf3bc1ab646a9daad45f94342
SherryChou <sherry18030@gmail.com>
2016-05-06 01:36:55 +0800

    Basic GUI without any function

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 /**
@@ -8,16 +10,79 @@ import processing.core.PApplet;
 */
 public class Character {
 	
-	private MainApplet parent;
-
-	public Character(MainApplet parent){
-
+	public float x, y, radius=40;
+	private String name;
+	private PApplet parent;
+	private int value;
+	private int[] color = new int[3];
+	private ArrayList<Character> targets = new ArrayList<Character>();
+	private boolean showName;
+	
+	/*
+	 * Store these variables when instance created.
+	 */
+	public Character(PApplet parent, String name, float x, float y, int value, String color){
 		this.parent = parent;
-		
-	}
-
-	public void display(){
-
+		this.name = name;
+		this.x = x;
+		this.y = y;
+		this.value = value;
+		this.color = convertHexColor(color);
 	}
 	
+	private int[] convertHexColor(String color){
+		int[] colorRGB = new int[3]; 
+		colorRGB[0] = Integer.valueOf(color.substring(3, 4), 16)*255/16;
+		colorRGB[1] = Integer.valueOf(color.substring(5, 6), 16)*255/16;
+		colorRGB[2] = Integer.valueOf(color.substring(7, 8), 16)*255/16;
+		return colorRGB;
+	}
+	/*
+	 * Use display() to draw the character on the sketch.
+	 */
+	public void display(){
+		update();
+		this.parent.fill(this.color[0], this.color[1], this.color[2], 180);
+		this.parent.stroke(0, 0);
+		this.parent.ellipse(x, y, radius, radius);
+		
+		if(showName){
+			this.parent.fill(2, 202, 119, 255);
+			this.parent.rect(x, y+5, 120, 20);
+			this.parent.fill(255, 255);
+			this.parent.textSize(14);
+			this.parent.text(this.name,x+20,y+20);
+		}
+//		this.parent.fill(0);
+	}
+	
+	boolean overCircle(float x, float y, float diameter) {
+		  float disX = x - this.parent.mouseX;
+		  float disY = y - this.parent.mouseY;
+		  if(PApplet.sqrt(PApplet.sq(disX) + PApplet.sq(disY)) < diameter/2 ) {
+		    return true;
+		  } else {
+		    return false;
+		 }
+	}
+	
+	void update() {
+		  if(overCircle(this.x, this.y, this.radius) ) {
+		    showName = true;
+		  }
+		  else{
+			 showName = false;
+		  }
+	}
+	
+	/*
+	 * Add the target to the array list when loading file.
+	 */
+	public void addTarget(Character target, int value){
+		targets.add(target);
+	}
+	
+	public ArrayList<Character> getTargets(){
+		return this.targets;
+	}
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -13,23 +13,54 @@ import processing.data.JSONObject;
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
 	private String path = "main/resources/";
-	private String[] file = {"starwars-episode-1-interactions.json", "starwars-episode-2-interactions.json", 
+	private String[] file = {"", "starwars-episode-1-interactions.json", "starwars-episode-2-interactions.json", 
 							"starwars-episode-3-interactions.json", "starwars-episode-4-interactions.json", 
 							"starwars-episode-5-interactions.json", "starwars-episode-6-interactions.json", 
 							"starwars-episode-7-interactions.json"};
 	
 	private final static int width = 1200, height = 650;
 	private ArrayList<Character> characters;
+	private int[] characterNum = new int[8];
 	
 	public void setup() {
 		size(width, height);
 		characters = new ArrayList<Character>();
+		characterNum[0] = 0;
 		smooth();
 		loadData();
 	}
 
 	public void draw() {
 		background(255);
+		int i;
+		// title
+		textSize(30);
+		text("Star Wars",width/2-70, 30);
+		
+		// big circle
+		strokeWeight(4);
+		stroke(132, 219, 0);
+		fill(0,0);
+		ellipse(width/2, height/2, 400, 400);
+		
+		// button ADD ALL
+		fill(2, 202, 119, 255);
+		stroke(0, 0);
+		rect(width*3/4, 40, 150, 50);
+		fill(255,255);
+		textSize(20);
+		text("ADD ALL",width*3/4+30, 70);
+		
+		// button CLEAR
+		fill(2, 202, 119, 255);
+		rect(width*3/4, 140, 150, 50);
+		fill(255,255);
+		textSize(20);
+		text("CLEAR",width*3/4+40, 170);
+		
+		for(i=this.characterNum[0]; i<this.characterNum[1]; i++){
+			characters.get(i).display();
+		}
 	}
 	
 	/* Change episode */
@@ -56,16 +87,22 @@ public class MainApplet extends PApplet{
 			
 		}
 	}
+	
 
 	private void loadData(){
 		JSONArray nodes, links;
 		JSONObject data, tmp;
 		int i, episode;
+		float x=40, y=40;
 		
-		for(episode=0; episode<7; episode++){
+		for(episode=1; episode<=7; episode++){
 			data = loadJSONObject(path+file[episode]);
 			nodes = data.getJSONArray("nodes");
 			links = data.getJSONArray("links");
+			
+			// record the number of characters of each episode
+			this.characterNum[episode] = nodes.size() + this.characterNum[episode-1];
+			
 			for(i=0; i<nodes.size(); i++){
 				tmp = nodes.getJSONObject(i);
 				
@@ -73,14 +110,20 @@ public class MainApplet extends PApplet{
 				int v = tmp.getInt("value");
 				String c = tmp.getString("colour");
 				
-	//			characters.add(new Character(this, n, x, y));
+				characters.add(new Character(this, n, x, y, v, c));
+				x += 60;
+				// next line
+				if(x >= 240){
+					y += 60;
+					x = 40;
+				}
 			}
 			for(i=0; i<links.size(); i++){
 				tmp = links.getJSONObject(i);
 				int s = tmp.getInt("source");
 				int t = tmp.getInt("target");
 				int v = tmp.getInt("value");
-	//			characters.get(s).addTarget(characters.get(t));
+				characters.get(s).addTarget(characters.get(t), v);
 			}
 		}
 	}
